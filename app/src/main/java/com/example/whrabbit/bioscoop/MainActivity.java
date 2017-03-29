01ce76a3d3bd715cbcc6ff2ca52585b1d5a4d2af
Whrabbit <whitneyrabbitc@gmail.com>
2017-03-29 23:42:48 +0200

    more screens/homescreen buttons linked
    all homescreen buttons are linked with new screens

--- app/src/main/java/com/example/whrabbit/bioscoop/MainActivity.java
+++ app/src/main/java/com/example/whrabbit/bioscoop/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.whrabbit.bioscoop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button filmBtn, biosBtn, ticketBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        filmBtn = (Button) findViewById(R.id.filmBtn);
+        filmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), FilmList.class);
+                startActivity(i);
+            }
+        });
+
+
+        biosBtn = (Button) findViewById(R.id.biosBtn);
+        biosBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Contact.class);
+                startActivity(i);
+            }
+        });
+
+
+        ticketBtn = (Button) findViewById(R.id.ticketBtn);
+        ticketBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Ticket.class);
+                startActivity(i);
+            }
+        });
     }
 }
