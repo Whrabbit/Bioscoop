7f7dd00d97dca9de007875066bdccc8eccbb7ba2
Whrabbit <whitneyrabbitc@gmail.com>
2017-03-30 15:08:12 +0200

    methode toegevoegd

--- app/src/main/java/com/example/whrabbit/bioscoop/MainActivity.java
+++ app/src/main/java/com/example/whrabbit/bioscoop/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.whrabbit.bioscoop;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button filmBtn, biosBtn, ticketBtn;
 
-    String potato
+    String potato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,4 +46,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    
+    public void testMethode(){
+        Log.e("potato", "potato");
+    }
 }
+
