15fc263375184750625adb5f90c390986cfe3b45
Zandkorrel <Zandkorrel@users.noreply.github.com>
2017-03-31 21:53:58 +0200

    Fixed ID's

--- app/src/main/java/com/example/whrabbit/bioscoop/FilmList.java
+++ app/src/main/java/com/example/whrabbit/bioscoop/FilmList.java
@@ -17,18 +17,18 @@ public class FilmList extends AppCompatActivity implements AdapterView.OnItemSel
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_film_list);
 
-        filterSpin = (Spinner) findViewById(R.id.filterSpin);
-        ArrayAdapter a = ArrayAdapter.createFromResource(this, R.array.filterSpinStrings,android.R.layout.simple_spinner_item);
+        filterSpin = (Spinner) findViewById(R.id.filterTypeSpinner);
+        ArrayAdapter a = ArrayAdapter.createFromResource(this, R.array.filterTypeSpinnerStrings,android.R.layout.simple_spinner_item);
         filterSpin.setAdapter(a);
         filterSpin.setOnItemSelectedListener(this);
 
-        kiesSpin = (Spinner) findViewById(R.id.kiesSpin);
-        ArrayAdapter b = ArrayAdapter.createFromResource(this, R.array.kiesSpinStrings,android.R.layout.simple_spinner_item);
+        kiesSpin = (Spinner) findViewById(R.id.filterSpinner);
+        ArrayAdapter b = ArrayAdapter.createFromResource(this, R.array.filterSpinnerStrings,android.R.layout.simple_spinner_item);
         kiesSpin.setAdapter(b);
         kiesSpin.setOnItemSelectedListener(this);
 
-        orderSpin = (Spinner) findViewById(R.id.orderSpin);
-        ArrayAdapter c = ArrayAdapter.createFromResource(this, R.array.orderSpinStrings,android.R.layout.simple_spinner_item);
+        orderSpin = (Spinner) findViewById(R.id.sortBySpinner);
+        ArrayAdapter c = ArrayAdapter.createFromResource(this, R.array.sortBySpinnerStrings,android.R.layout.simple_spinner_item);
         orderSpin.setAdapter(c);
         orderSpin.setOnItemSelectedListener(this);
     }
