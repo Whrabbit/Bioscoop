e087314e0319f7b6f3e9c28a9fcd320d124aa391
Zandkorrel <Zandkorrel@users.noreply.github.com>
2017-03-31 13:43:37 +0200

    Updated Film Class

--- app/src/main/java/com/example/whrabbit/bioscoop/API/Film.java
+++ app/src/main/java/com/example/whrabbit/bioscoop/API/Film.java
@@ -1,8 +1,98 @@
 package com.example.whrabbit.bioscoop.API;
 
+import java.util.ArrayList;
+
 /**
  * Created by Koen Kamman on 30-3-2017.
  */
 
 public class Film {
+
+    private String poster_path, overview, release_date, original_title, original_language, title, backdrop_path;
+    private ArrayList<Integer> genre_ids;
+    private ArrayList<String> genres;
+
+    public Film() {
+        poster_path = null;
+        overview = "";
+        release_date = "";
+        original_title = "";
+        original_language = "";
+        title = "";
+        backdrop_path = null;
+        genre_ids = new ArrayList<>();
+        genres = new ArrayList<>();
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public void setOriginal_title(String original_title) {
+        this.original_title = original_title;
+    }
+
+    public String getOriginal_language() {
+        return original_language;
+    }
+
+    public void setOriginal_language(String original_language) {
+        this.original_language = original_language;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBackdrop_path() {
+        return backdrop_path;
+    }
+
+    public void setBackdrop_path(String backdrop_path) {
+        this.backdrop_path = backdrop_path;
+    }
+
+    public ArrayList<Integer> getGenre_ids() {
+        return genre_ids;
+    }
+
+    public void setGenre_ids(ArrayList<Integer> genre_ids) {
+        this.genre_ids = genre_ids;
+    }
+
+    public ArrayList<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(ArrayList<String> genres) {
+        this.genres = genres;
+    }
 }
